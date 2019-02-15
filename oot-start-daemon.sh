b06030e6b7074b6f58957de7a4e4a64ca6045b73
Emmanuel <emmanuel.utrum@pm.me>
2019-02-15 02:14:13 +0000

    new parameters

--- oot-start-daemon.sh
+++ oot-start-daemon.sh
@@ -10,7 +10,7 @@ SEEDIP=78.47.196.146
 docker run -d --rm --name $AC_NAME -ti \
   --net host \
   --mount "src=$AC_NAME,dst=/home/komodo/" \
-  kmdplatform/komodod \
+  kmdplatform/komodod:sapling_lite \
   -rpcuser=$USER \
   -rpcpassword=$PASSWD \
   -bind=0.0.0.0 \
@@ -19,6 +19,7 @@ docker run -d --rm --name $AC_NAME -ti \
   -whitelist=127.0.0.1 \
   -ac_name=$AC_NAME \
   -ac_supply=$AC_SUPPLY \
+  -ac_sapling=5000000 \
   -addnode=$SEEDIP \
   -printtoconsole=1 \
   -server=1 \
@@ -31,4 +32,3 @@ docker run -d --rm --name $AC_NAME -ti \
   -zmqpubrawtx='tcp://127.0.0.1:8332' \
   -zmqpubhashblock='tcp://127.0.0.1:8332' \
   -reindex
-
